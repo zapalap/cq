803723be74bf02094d5c341dc005a65bdc8207d3
Przemyslaw Zapala <przemyslaw.zapala@capgemini.com>
2016-09-15 09:36:43 +0200

    Improve logging

--- cq/Infrastructure/CommonHandlers/ResponseLoggingHandler.cs
+++ cq/Infrastructure/CommonHandlers/ResponseLoggingHandler.cs
@@ -9,11 +9,11 @@ using System.Web;
 
 namespace cq.Infrastructure.CommonHandlers
 {
-    public class ConsoleLoggingHandler<TRequest, TResponse> : IPostRequestHandler<TRequest, TResponse>
+    public class ResponseLoggingHandler<TRequest, TResponse> : IPostRequestHandler<TRequest, TResponse>
     {
         public void Handle(TRequest request, TResponse response)
         {
-            var logEntry = $"{typeof(TRequest).Name} => {JsonConvert.SerializeObject(response)}";
+            var logEntry = $"{typeof(TRequest).Name} in {JsonConvert.SerializeObject(request)} out {JsonConvert.SerializeObject(response)}";
 
             Debug.Write(logEntry);
         }
